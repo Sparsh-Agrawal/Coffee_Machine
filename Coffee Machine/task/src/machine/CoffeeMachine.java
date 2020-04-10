78ac92b2ab3b6ed68fc6f73ccd72c11a3cfea683
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-04-10 17:08:07 +0530

    stage 3 completed

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -6,19 +6,50 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        int w = scan.nextInt() / 200;
+
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int m =scan.nextInt() / 50;
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int cb = scan.nextInt() / 15;
+
         System.out.println("Write how many cups of coffee you will need: ");
+        int cc = scan.nextInt();
 
-        // taking input
-        int n = scan.nextInt();
+        int cm = w;
 
-        int w = 200 * n;
-        int m = 50 * n;
-        int cb = 15 * n;
+        if( m < cm && m < cb ) {
+            cm = m;
+        } else if (cb < cm ) {
+            cm = cb;
+        }
 
-        System.out.println("For " + n + "cups of coffee you will need:");
-        System.out.println(w + " ml of water");
-        System.out.println(m + " ml of milk");
-        System.out.println(cb + " g of coffee beans");
+        if ( cm == cc ) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if ( cc < cm ) {
+            System.out.println("Yes, I can make that amount of coffee (and even " + (cm-cc) + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + (cm) + " cup(s) of coffee");
+        }
+
+
+
+
+//        System.out.println("Write how many cups of coffee you will need: ");
+//
+//        // taking input
+//        int n = scan.nextInt();
+//
+//        int w = 200 * n;
+//        int m = 50 * n;
+//        int cb = 15 * n;
+//
+//        System.out.println("For " + n + "cups of coffee you will need:");
+//        System.out.println(w + " ml of water");
+//        System.out.println(m + " ml of milk");
+//        System.out.println(cb + " g of coffee beans");
 
     }
 }
